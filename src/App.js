41f7d328499011378fe8365cf3545eca03a586cc
Irina Gabuaeva <irina@macbook-pro.lan>
2021-11-11 21:18:33 -0500

    create: Screen and Note components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useDrag, useDrop, DndProvider } from 'react-dnd'
+import { HTML5Backend } from "react-dnd-html5-backend";
+import {useState} from 'react'
 
-function App() {
+function Screen() {
+  const [listOfNotes, setListOfNotes] = useState([
+    {
+    position: {
+      x: 0,
+      y: 0,
+    },
+    text: "Hello"
+  },
+    {
+      position: {
+        x: 200,
+        y: 60,
+      },
+      text: "" 
+    },
+    {
+      position: {
+        x: 100,
+        y: 100,
+      },
+      text: "" 
+    }
+  ])
+  console.log("listOfNotes", listOfNotes)
+  const createNote = () => {
+    const newNote = {
+      position: {
+        x: 0,
+        y: 0,
+      },
+      text: ""
+    }
+   setListOfNotes(listOfNotes => [...listOfNotes, newNote])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Screen">
+      {listOfNotes ? listOfNotes.map((note, id) => {
+      return (<Note note={note} key={id} />)
+    }): ""}
+      
+      <button onClick={() => createNote()}>New Note</button>
     </div>
   );
 }
 
-export default App;
+function Note(props) {
+  console.log("props", props)
+  const {position, text} = props.note
+  return (
+    <div className="Note" style={{ left: `${position.x}px`, top: `${position.y}px`}}>{text}</div>
+  )
+}
+export default Screen;
