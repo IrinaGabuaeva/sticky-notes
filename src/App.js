e36170269a573e41835f4c7c926cb0ea85f5f6d0
Irina Gabuaeva <irina@MacBook-Pro.fios-router.home>
2021-11-14 00:35:53 -0500

    implement drag and drop

--- src/App.js
+++ src/App.js
@@ -1,60 +1,102 @@
-import './App.css';
-import { useDrag, useDrop, DndProvider } from 'react-dnd'
-import { HTML5Backend } from "react-dnd-html5-backend";
-import {useState} from 'react'
+import "./App.css";
+import { useDrag, useDrop } from "react-dnd";
+import { useState, useEffect } from "react";
 
 function Screen() {
-  const [listOfNotes, setListOfNotes] = useState([
-    {
-    position: {
-      x: 0,
-      y: 0,
-    },
-    text: "Hello"
-  },
-    {
-      position: {
-        x: 200,
-        y: 60,
-      },
-      text: "" 
-    },
-    {
-      position: {
-        x: 100,
-        y: 100,
-      },
-      text: "" 
-    }
-  ])
-  console.log("listOfNotes", listOfNotes)
+  const [listOfNotes, setListOfNotes] = useState([]);
+  console.log("listOfNotes", listOfNotes);
   const createNote = () => {
     const newNote = {
       position: {
         x: 0,
         y: 0,
       },
-      text: ""
-    }
-   setListOfNotes(listOfNotes => [...listOfNotes, newNote])
-  }
+      text: "",
+    };
+    setListOfNotes((listOfNotes) => [...listOfNotes, newNote]);
+  };
+
+  const moveNote = (id, position) => {
+    const newList = [...listOfNotes];
+    newList[id] = position;
+    setListOfNotes(newList);
+  };
+
+  const [{ getItem, didDrop, getSourceClientOffset }, drop] = useDrop(
+    () => ({
+      accept: "NOTE",
+      drop: (item, monitor) => {
+        // console.log(
+        //   "monitor.getSourceClientOffset()",
+        //   monitor.getSourceClientOffset()
+        // );
+        const coordinates = monitor.getSourceClientOffset();
+        console.log("coordinates", coordinates);
+        // setListOfNotes((listOfNotes[item.id] = coordinates));
+        console.log("LIST", listOfNotes[item.id]);
+        moveNote(item.id, coordinates);
+      },
+      // collect: (monitor) => ({
+      //   didDrop: !!monitor.didDrop(),
+      //   getItem: monitor.getItem(),
+      //   getSourceClientOffset: monitor.getSourceClientOffset(),
+      // }),
+    }),
+    [moveNote]
+  );
+
+  console.log("item!!", getItem);
+  console.log("didDrop", didDrop);
+
+  // console.log("getClientOffset", getClientOffset);
+  console.log("getSourceClientOffset()", getSourceClientOffset);
+
+  // useEffect(() => {
+  //   const position = {};
+  //   const setFromEvent = (e) =>
+  //     console.log("IN USE EFFECT", { x: e.clientX, y: e.clientY });
+
+  //   window.addEventListener("mousemove", setFromEvent);
+  //   return () => {
+  //     window.removeEventListener("mousemove", setFromEvent);
+  //   };
+  // }, [didDrop]);
 
   return (
-    <div className="Screen">
-      {listOfNotes ? listOfNotes.map((note, id) => {
-      return (<Note note={note} key={id} />)
-    }): ""}
-      
+    <div className="Screen" ref={drop}>
+      {didDrop ? moveNote(getItem.id, getSourceClientOffset) : ""}
+      {listOfNotes
+        ? listOfNotes.map((note, id) => {
+            return <Note note={note} key={id} id={id} />;
+          })
+        : ""}
+
       <button onClick={() => createNote()}>New Note</button>
     </div>
   );
 }
 
 function Note(props) {
-  console.log("props", props)
-  const {position, text} = props.note
+  // console.log("props", props);
+  const { x, y, text } = props.note;
+  const id = props.id;
+  const [, drag] = useDrag(() => ({
+    type: "NOTE",
+    item: { id },
+
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+  // console.log("monitor on draggable", monitor);
+  // console.log("item on draggable", item);
+  // console.log("collect on draggable", collect);
+  console.log("drag", drag);
   return (
-    <div className="Note" style={{ left: `${position.x}px`, top: `${position.y}px`}}>{text}</div>
-  )
+    <div className="Note" style={{ left: `${x}px`, top: `${y}px` }} ref={drag}>
+      {text}
+    </div>
+  );
 }
+
 export default Screen;
