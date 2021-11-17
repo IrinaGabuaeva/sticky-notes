5df81466abeadafe9a2a9b0d9c24edeb8feba33b
Irina Gabuaeva <irina@macbook-pro.lan>
2021-11-17 15:32:02 -0500

    change state data structure from array to object, fix text bug

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import { useState, useEffect, useRef } from "react";
 
 function Screen() {
   const defaultList =
-    JSON.parse(window.localStorage.getItem("listOfNotes")) ?? [];
-  let [listOfNotes, setLocalList] = useState(defaultList);
+    JSON.parse(window.localStorage.getItem("listOfNotes")) ?? {};
+
+  const [listOfNotes, setListOfNotes] = useState(defaultList);
   const listOfNotesRef = useRef(listOfNotes);
 
-  const setListOfNotes = (listOfNotes) => {
-    window.localStorage.setItem("listOfNotes", JSON.stringify(listOfNotes));
-    listOfNotesRef.current = listOfNotes;
-    setLocalList(listOfNotes);
+  const setAllNotes = (list) => {
+    window.localStorage.setItem("listOfNotes", JSON.stringify(list));
+    // listOfNotesRef.current = list;
   };
 
+  const noteIds = Object.keys(listOfNotes);
+  const lastNoteId = noteIds[noteIds.length - 1];
+  let currentId = lastNoteId ? parseInt(lastNoteId) + 1 : 1;
   const createNote = (x, y) => {
     x -= 125;
     y -= 125;
@@ -24,7 +27,10 @@ function Screen() {
       },
       text: "",
     };
-    setListOfNotes([...listOfNotesRef.current, newNote]);
+    listOfNotesRef.current[currentId.toString()] = newNote;
+    setAllNotes(listOfNotesRef.current);
+    setListOfNotes((prev) => ({ ...prev, [currentId.toString()]: newNote }));
+    currentId++;
   };
 
   useEffect(() => {
@@ -36,7 +42,7 @@ function Screen() {
   }, []);
 
   const moveNote = (id, position) => {
-    const newList = [...listOfNotes];
+    const newList = { ...listOfNotes };
     newList[id].position = position;
     setListOfNotes(newList);
   };
@@ -46,45 +52,48 @@ function Screen() {
       accept: "NOTE",
       drop: (item, monitor) => {
         const position = monitor.getSourceClientOffset();
-        moveNote(item.id, position, item.noteText);
+        moveNote(item.id, position);
       },
     }),
     [moveNote]
   );
 
   const deleteNote = (id) => {
-    const newList = listOfNotes
-      .slice(0, id)
-      .concat(listOfNotes.slice(id + 1, listOfNotes.length));
+    delete listOfNotes[id];
+    const newList = { ...listOfNotes };
     setListOfNotes(newList);
+    setAllNotes(newList);
   };
 
   const clearAll = () => {
-    setListOfNotes([]);
+    setListOfNotes({});
+    setAllNotes({});
   };
+
   const saveText = (id, text) => {
-    const newList = [...listOfNotes];
+    const newList = { ...listOfNotes };
     newList[id].text = text;
     setListOfNotes(newList);
+    setAllNotes(newList);
   };
+
   return (
     <div className="Screen" ref={drop}>
       <h3 className="Text">
         Double click anywhere on the screen to create a note
       </h3>
-      {listOfNotes
-        ? listOfNotes.map((note, id) => {
-            return (
-              <Note
-                note={note}
-                key={id}
-                id={id}
-                deleteNote={deleteNote}
-                saveText={saveText}
-              />
-            );
-          })
-        : ""}
+      {console.log("listOfNotes in render", listOfNotes)}
+      {Object.keys(listOfNotes).map((id) => {
+        return (
+          <Note
+            id={id}
+            note={listOfNotes[id]}
+            key={id}
+            deleteNote={deleteNote}
+            saveText={saveText}
+          />
+        );
+      })}
       <button
         className="ClearButton"
         style={{ height: "30px" }}
@@ -97,24 +106,26 @@ function Screen() {
 }
 
 function Note(props) {
-  console.log("PROPS", props);
+  console.log("Props:", props);
   const { x, y } = props.note.position;
   const { text } = props.note;
-  console.log("text", text);
-  const id = props.id;
-  console.log("id", id);
+  const { id } = props;
+
   const [noteText, setText] = useState(text);
   const [clicked, setClicked] = useState(false);
-  console.log("noteText", noteText);
+
   const [, drag] = useDrag(() => ({
     type: "NOTE",
     item: { id, noteText },
   }));
 
   const saveText = () => {
-    console.log("BLURR");
     setClicked(false);
     props.saveText(id, noteText);
+  };
+
+  const deleteNote = (id) => {
+    props.deleteNote(id);
   };
 
   return (
@@ -135,12 +146,12 @@ function Note(props) {
             e.target.value = val;
           }}
           onChange={(e) => setText(e.target.value)}
-          onMouseLeave={() => saveText()}
+          onBlur={() => saveText()}
         />
       ) : (
         <div className="NoteText">{noteText}</div>
       )}
-      <button className="DeleteButton" onClick={() => props.deleteNote(id)}>
+      <button className="DeleteButton" onClick={() => deleteNote(id)}>
         X
       </button>
     </div>
